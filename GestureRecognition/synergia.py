84b58730eb19b24bbe5aeae3b9c841f19a50ad95
DESKTOP-IO708AN\Bruger <hammerjakob@gmail.com>
2020-08-12 13:51:38 +0200

    add osc implementation and prediction adj

--- GestureRecognition/synergia.py
+++ GestureRecognition/synergia.py
@@ -3,10 +3,12 @@ import tensorflow as tf
 import numpy as np
 import sys
 
+from datetime import datetime
+
 from Models.synergia_classifier import SynergiaClassifier
 
 class Synergia:
-    def __init__(self, model_filename, buffer_size=100, min_buffer_size=10):
+    def __init__(self, model_filename, buffer_size=17, min_buffer_size=7):
         self._model = self._init_model(model_filename)
 
         self._expected_joints = ["RightShoulder", "RightElbow", "RightWrist", "LeftShoulder", "LeftElbow", "LeftWrist"]
@@ -16,7 +18,9 @@ class Synergia:
         self._buffer = Queue(maxsize=buffer_size)
         self._min_buffer_size = min_buffer_size
 
-        self.predictions = []
+        # self.predictions = []
+        self._latest_label = "nothing"
+        self._latest_confidence = -1
 
     def _init_model(self, filename):
         model = SynergiaClassifier(
@@ -46,15 +50,15 @@ class Synergia:
 
     def predict(self, obj, joint):
         if joint not in self._expected_joints: #skip joint you dont care about
-            print(f"- got {joint} skipping...")
-            return
+            # print(f"- got {joint} skipping...")
+            return self._latest_label, self._latest_confidence
         
         if joint != self._expected_joints[self._expected_pos]: #received desired joint, but there was a skip
-            print(f"- got {joint}, expected {self._expected_joints[self._expected_pos]}, reseting timestep...")
+            # print(f"- got {joint}, expected {self._expected_joints[self._expected_pos]}, reseting timestep...")
             self._expected_pos = 0
             self._current_time_step = []
-            return
-        
+            return self._latest_label, self._latest_confidence      
+
         self._current_time_step.append({
                 "x":obj["x"],
                 "y":obj["y"],
@@ -66,21 +70,23 @@ class Synergia:
             self._expected_pos = 0
 
             if self._buffer.full():
-                print(f"-- queue is full - removeing oldest obj...")
+                # print(f"-- queue is full - removeing oldest obj...")
                 self._buffer.get()
             self._buffer.put(self._current_time_step)
-            print(f"- got {joint}, adding to queue, queue obj size:{len(self._current_time_step)}, queue size:{self._buffer.qsize()}")
+            # print(f"- got {joint}, adding to queue, queue obj size:{len(self._current_time_step)}, queue size:{self._buffer.qsize()}")
             self._current_time_step = []
         else:
-            print(f"- got {joint}, seeking next time step... ")
+            # print(f"- got {joint}, seeking next time step... ")
             self._expected_pos += 1
-            return
+            return self._latest_label, self._latest_confidence
         
         #time step added to the buffer -> process and predict
         if self._buffer.qsize() < self._min_buffer_size:
-            print("- queue is too small, skipping...")
+            return self._latest_label, self._latest_confidence
+            # print("- queue is too small, skipping...")
         else:
-            print("- predicting...")
+            # print("- predicting...")
+            start_time = datetime.now()
             x = self._process_buffer()
             x = tf.reshape(x, [x.shape[0], -1, 18])
             x = tf.constant(x, dtype=tf.float64)
@@ -92,9 +98,15 @@ class Synergia:
             y_pred_confidence = tf.math.reduce_max(predictions_last_ts, axis=1).numpy()[0]
 
             label = self._get_label_from_idx(y_pred)
-            self.predictions.append(label)
-            print()
-            print(f"-- predicted {label} with confidence {y_pred_confidence}, distribution:{predictions_last_ts.numpy()}")
+            end_time = datetime.now()
+            print(f"Took: {end_time-start_time}")
+
+            self._latest_label = label
+            self._latest_confidence = y_pred_confidence
+            # self.predictions.append(label)
+            # print()
+            # print(f"-- predicted {label} with confidence {y_pred_confidence}, distribution:{predictions_last_ts.numpy()}")
+            return self._latest_label, self._latest_confidence
 
 
     def _get_label_from_idx(self, label_idx):
--- GestureRecognition/test_synergia.py
+++ GestureRecognition/test_synergia.py
@@ -1,23 +1,53 @@
 import json
 import time
+import argparse
+import random
+
+from pythonosc import dispatcher
+from pythonosc import osc_server
 
 from synergia import Synergia
+synergia = Synergia("./TrainedModels/synergia_gesture_classifier.h5")
 
 with open("./Data/Attract/sample1.json") as f:
 # with open("./Data/Clap/clap1.json") as f:    
     data = json.load(f)
 
-synergia = Synergia("./TrainedModels/synergia_gesture_classifier.h5")
-
-for i,e in enumerate(data):
-
+def handleMessage(address, arg1, arg2, arg3, _):
     obj = {
-        "x":data[i]["x"],
-        "y":data[i]["y"],
-        "z":data[i]["z"]
+        "x":arg1,
+        "y":arg2,
+        "z":arg3
     }
-    joint = data[i]["joint"]
+    prediction, confidence = synergia.predict(obj, address[1:])
+    print(f"{prediction}, {confidence}")
+    # print(f"[{address[1:]}], {arg2}")
 
-    synergia.predict(obj, joint)
-    # time.sleep(0.01)
-print(synergia.predictions)
+# for i,e in enumerate(data):
+
+#     obj = {
+#         "x":data[i]["x"],
+#         "y":data[i]["y"],
+#         "z":data[i]["z"]
+#     }
+#     joint = data[i]["joint"]
+
+#     synergia.predict(obj, joint)
+#     # time.sleep(0.01)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip",
+        default="127.0.0.1", help="The ip to listen on")
+    parser.add_argument("--port",
+        type=int, default=7403, help="The port to listen on")
+    args = parser.parse_args()
+
+    dispatcher = dispatcher.Dispatcher()
+    dispatcher.map("*", handleMessage)
+
+    server = osc_server.ThreadingOSCUDPServer(
+        (args.ip, args.port), dispatcher)
+    print("Serving on {}".format(server.server_address))
+    server.serve_forever()
